4549decadd606799b82892d654d142652919daa8
abdullah ardah <abdullah.ardah2@hotmail.com>
2024-09-26 14:01:40 -0700

    book list and book  details

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+namespace Product_app.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; } = string.Empty;
+        public decimal BookPrice { get; set; }
+    }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace product__app.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
